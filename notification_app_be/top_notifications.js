1d5ddd26b36db0a910ec894eb081aadc4586d08c
sriramgatamaneni <sriramgatamaneni@gmail.com>
2026-05-02 13:03:23 +0530

    Update top_notifications.js

--- notification_app_be/top_notifications.js
+++ notification_app_be/top_notifications.js
@@ -1,7 +1,7 @@
 async function getNotifications() {
     const res = await fetch("http://20.207.122.201/evaluation-service/notifications", {
         headers: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzcmlyYW1fZ2F0YW1hbmVuaUBzcm1hcC5lZHUuaW4iLCJleHAiOjE3Nzc3MDY3MjMsImlhdCI6MTc3NzcwNTgyMywiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6IjBkYjUyMGEwLTFjOGQtNDlhMy04YzIyLTVmOGRlNWJkN2M4NyIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImdhdGFtYW5lbmkgc3JpcmFtIiwic3ViIjoiZDBlNjFlODktNDVjYy00NjI1LThmOWQtNWE2ZDRhODdkYjkxIn0sImVtYWlsIjoic3JpcmFtX2dhdGFtYW5lbmlAc3JtYXAuZWR1LmluIiwibmFtZSI6ImdhdGFtYW5lbmkgc3JpcmFtIiwicm9sbE5vIjoiYXAyMzExMDAxMTY2MyIsImFjY2Vzc0NvZGUiOiJRa2JweEgiLCJjbGllbnRJRCI6ImQwZTYxZTg5LTQ1Y2MtNDYyNS04ZjlkLTVhNmQ0YTg3ZGI5MSIsImNsaWVudFNlY3JldCI6InVEa1VDR3l2dVNFS3lkZ1cifQ.Y7WV1H4EXesfT4HGDrh150GJcurDmSNS3zZGQjW03h0"
+            "Authorization": "Bearer Token"
         }
     });
     return (await res.json()).notifications;

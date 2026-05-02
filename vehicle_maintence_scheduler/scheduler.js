2f9a500570c693e838e442f34a50adf997edf3a7
sriramgatamaneni <sriramgatamaneni@gmail.com>
2026-05-02 13:02:20 +0530

    Update scheduler.js

--- vehicle_maintence_scheduler/scheduler.js
+++ vehicle_maintence_scheduler/scheduler.js
@@ -6,7 +6,7 @@ async function getData() {
 
     const vehiclesRes = await fetch("http://20.207.122.201/evaluation-service/vehicles", {
         headers: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzcmlyYW1fZ2F0YW1hbmVuaUBzcm1hcC5lZHUuaW4iLCJleHAiOjE3Nzc3MDIwMTQsImlhdCI6MTc3NzcwMTExNCwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6ImFlOTYwMzdlLTQzOTMtNDcyNC04OGM4LWUwY2RlNWZiOWU5NSIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImdhdGFtYW5lbmkgc3JpcmFtIiwic3ViIjoiZDBlNjFlODktNDVjYy00NjI1LThmOWQtNWE2ZDRhODdkYjkxIn0sImVtYWlsIjoic3JpcmFtX2dhdGFtYW5lbmlAc3JtYXAuZWR1LmluIiwibmFtZSI6ImdhdGFtYW5lbmkgc3JpcmFtIiwicm9sbE5vIjoiYXAyMzExMDAxMTY2MyIsImFjY2Vzc0NvZGUiOiJRa2JweEgiLCJjbGllbnRJRCI6ImQwZTYxZTg5LTQ1Y2MtNDYyNS04ZjlkLTVhNmQ0YTg3ZGI5MSIsImNsaWVudFNlY3JldCI6InVEa1VDR3l2dVNFS3lkZ1cifQ.Qc5TscohFqtIu9aWksqt1-lz1AAbE7YzA3ACr_FStWg"
+            "Authorization": "Bearer Token"
         }
     });
     const vehicles = await vehiclesRes.json();
